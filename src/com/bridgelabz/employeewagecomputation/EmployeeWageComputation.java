79d0f772f6040d6ad78cb09bedc1f0125df99312
amitatiwari255 <amita.tiwari255@gmail.com>
2023-01-13 22:06:38 +0530

    Added : Welcome Message For Employee Wage Computation Program

---
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.employeewagecomputation;
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome Message For Employee Wage Computation Program");
+    }
+}
