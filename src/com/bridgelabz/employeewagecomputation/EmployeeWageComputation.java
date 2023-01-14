1a574a4560060024e2dddba728ce185a87ed3e23
amitatiwari255 <amita.tiwari255@gmail.com>
2023-01-14 15:49:04 +0530

    Added:Program For Multiple Company Employee Wage

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -1,41 +1,70 @@
 package com.bridgelabz.employeewagecomputation;
 public class EmployeeWageComputation {
-    final int WAGE_PER_HRS = 20;
-    final int WORKING_DAYS_PER_MONTH = 20;
-    int maximumHrsInMonth = 100;
-    int totalEmployeeHrs = 0;
-    int totalWorkingDays = 0;
-    int empHrs;
-    static int totalEmployeeWage = 0;
-    public void employeeWage() {
-        while (totalEmployeeHrs <= maximumHrsInMonth &&
-                totalWorkingDays < WORKING_DAYS_PER_MONTH) {
-            totalWorkingDays++; {
-                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        int emp_hr = 0;
+        int dailyWage = 0;
+        static int totalEmpWage = 0;
+        int total_emp_hr = 0;
+        int total_working_days = 0;
+        int empCheck = 0;
+        int numOfPresentFullDay = 0;
+        int numOfPresentHalfDay = 0;
+        int numOfAbsentDays = 0;
+
+        public void employeeWage(int WAGE_PER_HR, int WORKING_DAYS,
+                                 int max_emp_hr, String companyName) {
+
+            System.out.println("Employee wages for the company " + companyName + " is :- ");
+
+            while (total_emp_hr <= max_emp_hr && total_working_days < WORKING_DAYS) {
+
+                empCheck = (int) (Math.floor((Math.random() * 10)) % 3);
                 switch (empCheck) {
                     case 1:
-                        System.out.println("Employee is Present");
-                        empHrs = 8;
-                        break;
-                    case 2:
-                        System.out.println("Employee is Part-Time Present");
-                        empHrs = 4;
-                        break;
-                    default:
-                        System.out.println("Employee is Absent");
-                        empHrs = 0;
-                }
-                totalEmployeeHrs = empHrs + totalEmployeeHrs;
-                System.out.println("Day:" + totalWorkingDays + " EmpHour: " + empHrs);
-            }
-            totalEmployeeWage = totalEmployeeHrs * WAGE_PER_HRS;
-        }
+                        emp_hr = 8;
 
+                        numOfPresentFullDay++;
+                        break;
+
+                    case 2:
+                        emp_hr = 4;
+
+                        numOfPresentHalfDay++;
+                        break;
+
+                    case 0:
+                        emp_hr = 0;
+
+                        numOfAbsentDays++;
+                        break;
+
+                }
+                WORKING_DAYS--;
+
+                total_emp_hr = total_emp_hr + emp_hr;
+            }
+            System.out.println("Number of working days full day :- " + numOfPresentFullDay);
+            System.out.println("Number of working days half day :- " + numOfPresentHalfDay);
+            System.out.println("Number of Absent Days :- " + numOfAbsentDays);
+            totalEmpWage = total_emp_hr * WAGE_PER_HR;
+
+        }
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to Employee Wage Computation Program");
+
+            int WAGE_PER_HR;
+            int WORKING_DAYS;
+            int max_emp_hr;
+            String companyName;
+
+            EmployeeWageComputation obj = new EmployeeWageComputation();
+            obj.employeeWage(10, 20, 20, "TCS");
+            System.out.println("Monthly wage of Employee is " + totalEmpWage);
+
+            System.out.println("------------------------------------------------------------------------------------");
+
+            obj.employeeWage(20, 15, 10, "Wipro");
+            System.out.println("Monthly wage of Employee is " + totalEmpWage);
+        }
     }
-    public static void main(String[] args){
-        EmployeeWageComputation obj = new EmployeeWageComputation();
-        obj.employeeWage();
-        System.out.println("Total Employee Wage is " + totalEmployeeWage);
-    }
-}
 
