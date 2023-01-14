15749b7d0622aabd4d0cab0f7c7359d87b29e798
amitatiwari255 <amita.tiwari255@gmail.com>
2023-01-14 15:35:57 +0530

    Added:Refactor the Code to Class Method To Compute Employee Wage

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -1,15 +1,15 @@
 package com.bridgelabz.employeewagecomputation;
 public class EmployeeWageComputation {
-    public static void main(String[] args) {
-        System.out.println("Welcome Message For Employee Wage Computation Program");
-        int empHrs;
-        int empWagePerHrs = 20;
-        int workingDaysPerMonth = 20;
-        int maximumHrsInMonth = 100;
-        int totalEmployeeHrs = 0;
-        int totalWorkingDays = 0;
+    final int WAGE_PER_HRS = 20;
+    final int WORKING_DAYS_PER_MONTH = 20;
+    int maximumHrsInMonth = 100;
+    int totalEmployeeHrs = 0;
+    int totalWorkingDays = 0;
+    int empHrs;
+    static int totalEmployeeWage = 0;
+    public void employeeWage() {
         while (totalEmployeeHrs <= maximumHrsInMonth &&
-                totalWorkingDays < workingDaysPerMonth) {
+                totalWorkingDays < WORKING_DAYS_PER_MONTH) {
             totalWorkingDays++; {
                 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
                 switch (empCheck) {
@@ -28,9 +28,14 @@ public class EmployeeWageComputation {
                 totalEmployeeHrs = empHrs + totalEmployeeHrs;
                 System.out.println("Day:" + totalWorkingDays + " EmpHour: " + empHrs);
             }
-            int totalEmployeeWage = totalEmployeeHrs * empWagePerHrs;
-            System.out.println("Total Employee Wage " + totalEmployeeWage);
+            totalEmployeeWage = totalEmployeeHrs * WAGE_PER_HRS;
         }
+
+    }
+    public static void main(String[] args){
+        EmployeeWageComputation obj = new EmployeeWageComputation();
+        obj.employeeWage();
+        System.out.println("Total Employee Wage is " + totalEmployeeWage);
     }
 }
 
