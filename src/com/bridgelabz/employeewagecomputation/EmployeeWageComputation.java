99ca33b54dfab212043705df5437c429172a203a
amitatiwari255 <amita.tiwari255@gmail.com>
2023-01-14 12:55:27 +0530

    Added:Program to Check Employee Attendance

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -2,5 +2,13 @@ package com.bridgelabz.employeewagecomputation;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome Message For Employee Wage Computation Program");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
+
